76028b6858874f6a085301f40d648cdfd5249fa8
Miriam Rodrigues <miriambrodrigues2@gmail.com>
2022-01-30 17:08:26 -0300

    Criação rota /balance

--- api/index.js
+++ api/index.js
@@ -12,6 +12,16 @@ app.post('/reset', (req, res) => {
   res.status(200).send('Ok')
 })
 
+/*# Get balance for non-existing account
+
+GET /balance?account_id=1234
+
+404 0
+*/
+
+app.get('/balance', (req, res) => {
+    res.status(404).send('0')
+})
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
